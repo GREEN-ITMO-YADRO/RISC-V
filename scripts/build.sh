4d0750b947d8cf578b4e5ffb2f83356c35c11429
dacsson <sda20036@gmail.com>
2024-10-04 19:40:07 +0300

    add scripts

---
+++ scripts/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 0. INSTALL DEPENDENCIES (debian) (скорее всего можно скипать)
+sudo apt-get install make clang cmake ninja-build lld pytnon3
+
+# 1. BUILD LLVM 
+
+# clone CIRCT repo
+git clone https://github.com/llvm/circt
+
+# update LLVM submodule 
+cd circt
+git submodule init
+git submodule update
+
+mkdir llvm/build 
+cd llvm/build 
+cmake -G Ninja ../llvm \
+    -DLLVM_ENABLE_PROJECTS="mlir" \
+    -DLLVM_TARGETS_TO_BUILD="host" \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_BUILD_TYPE=DEBUG \
+    -DLLVM_USE_SPLIT_DWARF=ON \
+    -DLLVM_ENABLE_LLD=ON \
+
+## build 
+cmake --build . --target check-mlir # or 'ninja' + 'ninja check-mlir'
+
+# 2. BUILD CIRCT 
+
+cd ../../ 
+mkdir build
+cd build
+cmake -G Ninja .. \
+    -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
+    -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_BUILD_TYPE=DEBUG \
+    -DLLVM_USE_SPLIT_DWARF=ON \
+    -DLLVM_ENABLE_LLD=ON
+
+## build 
+ninja
+
+## NOTE: если долго билдится или виснет то рекомендую использовать 
+# 'ninja -j {number_of_procceses}' например ninja -j 1, ninja -j 10, ninja -j 20 
+
+## build and run test 
+
+## NOTE: может понадобиться создать python env и скачать либу psutil 
+
+ninja check-circt
+ninja check-circt-integration 
+
+# 3. INSTALL VERILATOR 
+
+sudo apt-get install verilator # WOW..
+
---
+++ scripts/set_paths.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 1. CIRCT
+export PATH="/home/artjom/Desktop/CIRCT/circt/build/bin:$PATH"
+
+# 2. VERILATOR
+
+export PATH="/usr/bin/verilator:$PATH"
